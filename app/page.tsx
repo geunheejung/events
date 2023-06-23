82ad85156f6e71747ab3e0ff1ed39994a553f239
geunheejung <geunhee0212@gmail.com>
2023-06-23 15:08:30 +0900

    게시글 목록 스타일링.

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,44 @@
 import { getList } from "@/services/api";
 import { headers } from "next/dist/client/components/headers";
 import Link from "next/link";
+import "@/styles/post.scss";
+import { AiFillHeart, AiFillRedditCircle } from "react-icons/ai";
 
 export default async function Home() {
   const res = await getList();
+  const review = 9;
+  const username = "easylogic";
+  const likes = 125;
 
   return (
-    <div>
+    <div className="post-list-container">
       {res &&
         res.data.map((row) => {
           return (
-            <section key={row._id}>
-              <Link href={`/${row._id}`}>
-                <h3>{row.title}</h3>
-                <p>{row.content}</p>
-                <div>
-                  <span>{row.start_date}</span>
-                </div>
-              </Link>
-            </section>
+            <Link href={`/${row._id}`} key={row._id}>
+              <article key={row._id} className="post-item">
+                <section>
+                  <div className="header">{row.title}</div>
+                  <div className="content">{row.content}</div>
+                  <div className="info">
+                    <span className="date">2023년 6월 13일</span>
+                    <span>·</span>
+                    <span>{review}개의 댓글</span>
+                  </div>
+                </section>
+                <section className="footer">
+                  <div className="left">
+                    <AiFillRedditCircle style={{ width: 24, height: 24 }} />
+                    <span>by</span>
+                    <strong>{username}</strong>
+                  </div>
+                  <div>
+                    <AiFillHeart style={{ width: 24, height: 24 }} />
+                    {likes}
+                  </div>
+                </section>
+              </article>
+            </Link>
           );
         })}
     </div>
